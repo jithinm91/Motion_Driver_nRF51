46ca54ceb515b42a18211d1a07459f3afbf2aba0
Jithin <jithin@finrobotics.com>
2015-11-26 15:14:10 +0530

    [WIP] Added mpu9150.c and .h

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'motion_driver' 
+ * Target:  'nrf51422' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
